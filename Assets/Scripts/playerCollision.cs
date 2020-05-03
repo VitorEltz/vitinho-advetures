90d9e80220472d539284414017f47315b34d8641
VitorEltz <hethell7@gmail.com>
2020-05-03 12:37:05 -0300

    Collision Car and Restart Game

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class playerCollision : MonoBehaviour
+{
+    public PlayerMoviments movement;
+    public GameObject gameoverui;
+    public static bool gameispaused;
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager .GetActiveScene().buildIndex);
+        Time .timeScale = 1f;
+        gameispaused = false;
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision .collider .tag == "CarChallange")
+        {
+            movement .enabled = false;
+            gameoverui .SetActive(true);
+            Time .timeScale = 0f;
+            gameispaused = true;
+        }
+    }
+}
